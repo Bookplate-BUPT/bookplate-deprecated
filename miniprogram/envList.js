2c047e8c4478e12bc152be70d7a2d459002cd19d
QQK <67569413+KaseSerendipia@users.noreply.github.com>
2021-08-17 03:31:16 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"qqk-4gjankm535f1a524","alias":"qqk"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\微信web开发者工具\cli.bat" cloud functions deploy --e qqk-4gjankm535f1a524 --n quickstartFunctions --r --project "E:\学习\大创\SecondHandBooks" --report
