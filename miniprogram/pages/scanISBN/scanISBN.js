d1cf7893dbc63b4392e073f5fb604415034328ff
QQKdeGit <627948700@qq.com>
2021-09-10 15:36:05 +0800

    add scan ISBN code function

--- miniprogram/pages/scanISBN/scanISBN.js
+++ miniprogram/pages/scanISBN/scanISBN.js
@@ -3,42 +3,28 @@ Page({
 
   scanISBN() {
 
-    // wx.scanCode({
-    //   onlyFromCamera: false,
-    //   scanType: ['barCode'],
-    //   success: res => {
-    //     console.log('扫描ISBN码成功', res)
-    //     wx.cloud.callFunction({
-    //       name: 'getBookInfo',
-    //       data: {
-    //         isbn: res.result
-    //       },
-    //       success: res => {
-    //         console.log('调用云函数getBookInfo成功', res)
-    //       },
-    //       fail: err => {
-    //         console.log('调用云函数getBookInfo失败', err)
-    //       }
-    //     })
-    //   },
-    //   fail: err => {
-    //     console.log(err)
-    //   }
-    // })
-
-    wx.cloud.callFunction({
-      name: 'getBookInfo',
-      data: {
-        isbn: '9787510842344'
-      },
+    wx.scanCode({
+      onlyFromCamera: false,
+      scanType: ['barCode'],
       success: res => {
-        console.log('调用云函数getBookInfo成功', res)
+        console.log('扫描ISBN码成功', res)
+        wx.cloud.callFunction({
+          name: 'getBookInfo',
+          data: {
+            isbn: res.result
+          },
+          success: res => {
+            console.log('调用云函数getBookInfo成功', JSON.parse(res.result))
+          },
+          fail: err => {
+            console.log('调用云函数getBookInfo失败', err)
+          }
+        })
       },
       fail: err => {
-        console.log('调用云函数getBookInfo失败', err)
+        console.log(err)
       }
     })
   }
-
 
 })
