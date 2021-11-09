0f45f2768abda60ab61a8b0eab22bef2dd1a6f9d
QQKdeGit <627948700@qq.com>
2021-11-09 21:26:17 +0800

    feat: update main

--- miniprogram/pages/main/main.js
+++ miniprogram/pages/main/main.js
@@ -1,66 +1,39 @@
 // pages/main/main.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 关键字搜索
+  onSearch(event) {
+    if (event.detail.length > 0) {
+      wx.navigateTo({
+        url: '../searchResult/searchResult?keyword=' + event.detail,
+      })
+    } else {
+
+    }
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    // this.getTabBar().init();
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 扫描ISBN搜索
+  onScanSearch() {
+    wx.scanCode({
+      onlyFromCamera: false,
+      scanType: ['barCode'],
+      success: res => {
+        wx.navigateTo({
+          url: '../searchResult/searchResult?keyword=' + res.result,
+        })
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'error',
+          title: '识别失败',
+        })
+      }
+    })
   }
 })
