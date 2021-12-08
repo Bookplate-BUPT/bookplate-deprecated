64a04d560b97f584b65ebc935ae5b239edcda266
QQKdeGit <627948700@qq.com>
2021-12-08 14:03:31 +0800

    feat: update main page

--- miniprogram/pages/main/main.js
+++ miniprogram/pages/main/main.js
@@ -2,24 +2,56 @@
 Page({
 
   data: {
-
-
+    bookTypeOption: [
+      {
+        text: '全部书籍',
+        value: 0,
+      },
+      {
+        text: '大一',
+        value: 1,
+      },
+      {
+        text: '大二',
+        value: 2,
+      },
+    ],
+    sortTypeOption: [
+      {
+        text: '默认排序',
+        value: 0,
+      },
+      {
+        text: '最新上架',
+        value: 1,
+      },
+      {
+        text: '最多浏览',
+        value: 2,
+      },
+      {
+        text: '最多收藏',
+        value: 3,
+      },
+    ],
   },
 
   // 关键字搜索
-  onSearch(event) {
+  keySearch(event) {
     if (event.detail.length > 0) {
       wx.navigateTo({
         url: '../searchResult/searchResult?keyword=' + event.detail,
       })
     } else {
-
+      wx.showToast({
+        title: '内容不能为空',
+        icon: 'error'
+      })
     }
-
   },
 
   // 扫描ISBN搜索
-  onScanSearch() {
+  scanSearch() {
     wx.scanCode({
       onlyFromCamera: false,
       scanType: ['barCode'],
@@ -28,12 +60,14 @@ Page({
           url: '../searchResult/searchResult?keyword=' + res.result,
         })
       },
-      fail: err => {
+      fail: res => {
         wx.showToast({
           icon: 'error',
           title: '识别失败',
         })
       }
     })
-  }
+  },
+
+
 })
