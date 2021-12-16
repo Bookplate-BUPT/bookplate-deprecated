9b6f70924580fd77e176e449d761988a5f1db024
QQKdeGit <627948700@qq.com>
2021-12-16 20:37:31 +0800

    feat: can add books into cart now

--- miniprogram/pages/main/main.js
+++ miniprogram/pages/main/main.js
@@ -1,4 +1,6 @@
 // pages/main/main.js
+import __user from "../../utils/user"
+
 Page({
 
   data: {
@@ -102,8 +104,45 @@ Page({
   },
 
   // 添加商品到购物车
-  addGoodsToCart() {
-    console.log('添加购物车')
+  addGoodsToCart(event) {
+    if (!__user.checkLoginStatus()) {
+      wx.showToast({
+        title: '请先登录',
+        icon: 'error',
+      })
+    } else {
+      // 查询用户购物车里是否已有此商品
+      wx.cloud.database().collection('cart')
+        .where({
+          _openid: __user.getUserOpenid(),
+          goods_id: event.currentTarget.dataset.id,
+        })
+        .get()
+        .then(res => {
+          // 已经在购物车内
+          if (res.data.length) {
+            wx.showToast({
+              title: '已在购物车中',
+              icon: 'error',
+            })
+          } else {
+            // 不在购物车内
+            wx.cloud.database().collection('cart')
+              .add({
+                data: {
+                  goods_id: event.currentTarget.dataset.id,
+                  add_time: new Date(),
+                }
+              })
+              .then(res => {
+                wx.showToast({
+                  title: '添加成功',
+                  icon: 'success',
+                })
+              })
+          }
+        })
+    }
   },
 
   // 筛选书籍类型改变时调用
