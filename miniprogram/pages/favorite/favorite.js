4bd3d69d4dfa400e12b4146d6b90927a97d01ee6
QQKdeGit <627948700@qq.com>
2022-03-21 14:25:30 +0800

    feat: add favorite page

---
+++ miniprogram/pages/favorite/favorite.js
@@ -0,0 +1,16 @@
+// pages/favorite/favorite.js
+Page({
+  data: {
+    favoriteList: '',
+  },
+
+  onShow() {
+
+  },
+
+  // 获取已收藏的书籍
+  async getFavoriteList() {
+    // 可参照cart.js里的getCardList方法
+
+  }
+})
