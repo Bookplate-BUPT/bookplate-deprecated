da2c9444fc5735d54dff6b74a232e069dc7f90d8
QQKdeGit <627948700@qq.com>
2022-03-22 15:12:42 +0800

    feat: update seekBook page

--- miniprogram/pages/seekBook/seekBook.js
+++ miniprogram/pages/seekBook/seekBook.js
@@ -1,66 +1,20 @@
 // pages/seekBook/seekBook.js
+import __user from "../../utils/user"
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    // 书本信息相关
+    author: '',
+    introduction: '',   // 书本内容的介绍
+    isbn: '',
+    name: '',
+    publisher: '',
+    publishDate: '',
+    imageList: [],      // 将要上传至数据库的图片列表，类型为字符串数组
 
+    showList: [],       // 展示在页面上的图片列表，类型为对象数组
+    needs: '',
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
