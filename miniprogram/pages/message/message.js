80b8eb0cb77a3966c0b80cbc86b870e39f1ec0b1
QQKdeGit <627948700@qq.com>
2022-04-16 22:18:30 +0800

    feat: rebuild message page and remove buyer and seller

--- miniprogram/pages/message/message.js
+++ miniprogram/pages/message/message.js
@@ -1,64 +1,69 @@
 // pages/message/message.js
+import user from "../../utils/user";
 import __user from "../../utils/user"
 
 const app = getApp();
 
 Page({
   data: {
-    active: 0,
-    openid: '',
-
-    sellerList: [],   // 卖家列表
-    buyerList: [],    // 买家列表
+    userList: [],   // 用户列表
   },
 
   onLoad() {
-    this.setData({
-      openid: app.globalData.userOpenid,
-    })
-
-    this.getSellerList()
+    this.getUserList()
   },
 
-  // 获取卖家列表
-  async getSellerList() {
-    // 以买家为自己获取关系列表
-    const sellerList = await wx.cloud.database().collection('relationship')
-      .where({
-        buyer: this.data.openid
-      })
+  // 获取用户列表
+  async getUserList() {
+    // 获取关系列表
+    const relationshipList = await wx.cloud.database().collection('relationship')
+      .where(
+        wx.cloud.database().command.or([
+          {
+            user1: app.globalData.userOpenid,
+          },
+          {
+            user2: app.globalData.userOpenid,
+          }
+        ])
+      )
       .get()
 
-    // 根据卖家的openid查询卖家用户的详细信息
-    const promiseArray = await sellerList.data.map((i) => (
+    // 找出对方的openid，命名为otherid，并用于查找用户的详细资料
+    const userOpenidList = relationshipList.data.map((i) => ({
+      ...i,
+      otherid: i.user1 === app.globalData.userOpenid ? i.user2 : i.user1
+    }))
+
+    // console.log(userOpenidList)
+
+    // 根据otherid查询对方用户的详细信息
+    const promiseArray = await userOpenidList.map((i) => (
       wx.cloud.database().collection('users')
         .where({
-          _openid: i.seller
+          _openid: i.otherid
         })
-        .orderBy('last_conversation_time', 'desc')
         .get()
     ))
 
     // 等到所有的查询线程结束后再继续进行
-    const sellerDetailList = await Promise.all(promiseArray)
+    const userDetailList = await Promise.all(promiseArray)
 
-    // 将卖家的头像和昵称信息放入列表
-    const tempSellerList = sellerList.data.map((i, idx) => ({
-      ...i,
-      avatar: sellerDetailList[idx].data[0].avatarUrl,
-      nickName: sellerDetailList[idx].data[0].nickName,
+    // 将用户的头像和昵称信息放入列表
+    const tempUserList = userDetailList.map((i, idx) => ({
+      avatar: userDetailList[idx].data[0].avatarUrl,
+      nickName: userDetailList[idx].data[0].nickName,
+      otherid: userDetailList[idx].data[0]._openid,
+      last_content: userOpenidList[idx].last_content,
+      last_conversation_time: userOpenidList[idx].last_conversation_time,
     }))
 
+    // 按时间戳进行排序后再赋值
     this.setData({
-      sellerList: tempSellerList
+      userList: tempUserList.sort((x, y) => y.last_conversation_time - x.last_conversation_time)
     })
 
-    console.log(this.data.sellerList)
-  },
-
-  // 获取买家列表
-  getBuyerList() {
-
+    console.log(this.data.userList)
   },
 
   // 前往聊天室
