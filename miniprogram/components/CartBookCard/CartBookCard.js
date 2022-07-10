34cece9abaaa5fab2bfa15314e0eb3ea79d693c4
葛震鹏 <11231780+gezhenpeng@user.noreply.gitee.com>
2022-07-10 17:40:24 +0800

    refactor: add a cartbookcard custom component

---
+++ miniprogram/components/CartBookCard/CartBookCard.js
@@ -0,0 +1,57 @@
+// components/CartBookCard/CartBookCard.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    bookDetail: Object,
+    _id: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    // 进入商品详情页
+    goToBookDetail(event) {
+      wx.navigateTo({
+        url: '../bookDetail/bookDetail?id=' + event.currentTarget.dataset.id,
+      })
+    },
+
+    // 将商品移除购物车
+    deleteGoods(event) {
+      wx.cloud.database().collection('cart')
+        .doc(event.currentTarget.dataset.id)
+        .remove()
+        .then(res => {
+          wx.showToast({
+            title: '删除成功',
+            icon: 'success',
+          })
+
+          this.triggerEvent('delete', { _id: this.properties._id })
+        })
+        .catch(res => {
+          wx.showToast({
+            title: '删除失败',
+            icon: 'error',
+          })
+        })
+    },
+
+    // 购买商品，联系卖家
+    buyGoods(event) {
+      wx.navigateTo({
+        url: '../chatroom/chatroom?openid=' + event.currentTarget.dataset.openid,
+      })
+    },
+  }
+})
--- miniprogram/pages/cart/cart.js
+++ miniprogram/pages/cart/cart.js
@@ -118,45 +118,15 @@ Page({
     })
   },
 
-  // 进入商品详情页
-  goToBookDetail(event) {
-    wx.navigateTo({
-      url: '../bookDetail/bookDetail?id=' + event.currentTarget.dataset.id,
-    })
-  },
-
   // 将商品移除购物车
   deleteGoods(event) {
-    wx.cloud.database().collection('cart')
-      .doc(event.currentTarget.dataset.id)
-      .remove()
-      .then(res => {
-        wx.showToast({
-          title: '删除成功',
-          icon: 'success',
-        })
+    let tempCartList = this.data.cartList
+    const index = this.data.cartList.findIndex(i => i._id === event.detail._id)
 
-        let tempCartList = this.data.cartList
-        const index = this.data.cartList.findIndex(i => i._id === event.currentTarget.dataset.id)
+    tempCartList.splice(index, 1)
 
-        tempCartList.splice(index, 1)
-
-        this.setData({
-          cartList: tempCartList
-        })
-      })
-      .catch(res => {
-        wx.showToast({
-          title: '删除失败',
-          icon: 'error',
-        })
-      })
-  },
-
-  // 购买商品，联系卖家
-  buyGoods(event) {
-    wx.navigateTo({
-      url: '../chatroom/chatroom?openid=' + event.currentTarget.dataset.openid,
+    this.setData({
+      cartList: tempCartList
     })
-  },
+  }
 })
