0c93e5bae2a64aa6331601dd6fdb46a420a402ea
葛震鹏 <11231780+gezhenpeng@user.noreply.gitee.com>
2022-08-14 21:53:54 +0800

    fix: solve the issue that the picture still disappears after the picture is deleted and leaving mysellbook page

--- miniprogram/pages/sellBook/sellBook.js
+++ miniprogram/pages/sellBook/sellBook.js
@@ -1,18 +1,18 @@
 // pages/scanISBN/scanISBN.js
 
 const citys = {
-  '未来学院': ['电子信息类（元班）', '计算机类（元班）','通信工程','电子科学与技术','计算机科学与技术','网路空间安全'],
-  '电子工程学院': ['电子信息类','电子信息科学与技术','电子科学与技术','光电信息科学与工程'],
-  '计算机学院': ['计算机类','软件工程','计算机科学与技术','网络工程','数据科学与大数据技术'],
-  '信息与通信工程学院': ['通信工程（大类招生）','通信工程','电子信息工程','空间信息与数字技术'],
-  '网络空间安全学院': ['网络空间安全（大类招生）','网络空间安全','信息安全','密码科学与技术'],
-  '人工智能学院': ['人工智能（大类招生）','信息工程','人工智能','自动化','智能医学工程'],
-  '现代邮政学院': ['自动化类', '管理科学与工程类','机械工程','邮政工程','电子商务','邮政管理'],
-  '经济管理学院': ['大数据管理与应用金融科技','工商管理类','工商管理','公共事业管理'],
-  '理学院': ['理科试验班','数学与应用数学','信息与计算科学','应用物理学'],
-  '人文学院': ['英语','日语','法学'],
-  '数字媒体与设计艺术学院': ['智能交互设计', '数字媒体技术','数字媒体艺术','网络与新媒体'],
-  '国际学院': ['电信工程及管理','物联网工程','电子信息工程','智能科学与技术'],
+  '未来学院': ['电子信息类（元班）', '计算机类（元班）', '通信工程', '电子科学与技术', '计算机科学与技术', '网路空间安全'],
+  '电子工程学院': ['电子信息类', '电子信息科学与技术', '电子科学与技术', '光电信息科学与工程'],
+  '计算机学院': ['计算机类', '软件工程', '计算机科学与技术', '网络工程', '数据科学与大数据技术'],
+  '信息与通信工程学院': ['通信工程（大类招生）', '通信工程', '电子信息工程', '空间信息与数字技术'],
+  '网络空间安全学院': ['网络空间安全（大类招生）', '网络空间安全', '信息安全', '密码科学与技术'],
+  '人工智能学院': ['人工智能（大类招生）', '信息工程', '人工智能', '自动化', '智能医学工程'],
+  '现代邮政学院': ['自动化类', '管理科学与工程类', '机械工程', '邮政工程', '电子商务', '邮政管理'],
+  '经济管理学院': ['大数据管理与应用金融科技', '工商管理类', '工商管理', '公共事业管理'],
+  '理学院': ['理科试验班', '数学与应用数学', '信息与计算科学', '应用物理学'],
+  '人文学院': ['英语', '日语', '法学'],
+  '数字媒体与设计艺术学院': ['智能交互设计', '数字媒体技术', '数字媒体艺术', '网络与新媒体'],
+  '国际学院': ['电信工程及管理', '物联网工程', '电子信息工程', '智能科学与技术'],
   '北京邮电大学玛丽女王海南学院': ['信息与计算科学']
 };
 import __user from "../../utils/user"
@@ -35,6 +35,7 @@ Page({
 
     // 页面展示相关
     showList: [],       // 展示在页面上的图片列表，类型为对象数组
+    showListToo: [],    // 展示在页面上的图片列表，类型为对象数组, 原始图片数据
     showDifficultyOverLay: false,   // 遇到困难遮罩层的显示
     helpSteps: [        // 遇到困难遮罩层内容
       {
@@ -64,7 +65,7 @@ Page({
     ],
   },
   //选择书籍的类型时调用
-  selectClassification(e){
+  selectClassification(e) {
     this.setData({
       show: true
     })
@@ -75,13 +76,13 @@ Page({
     picker.setColumnValues(1, citys[value[0]]);
   },
 
-  onClose(){
+  onClose() {
     this.setData({
       show: false
     })
   },
 
-  onOrder(e){
+  onOrder(e) {
     var tempList = this.selectComponent('#classification').getValues()
     this.data.grade = tempList[0] + tempList[1]
     this.setData({
@@ -100,13 +101,14 @@ Page({
     }
     else {
       console.log(JSON.parse(options.message))
-      var { author, book_publish_date, description, grade, image_list, introduction, isbn, name, original_price, price, publisher, _id , college} = JSON.parse(options.message)
+      var { author, book_publish_date, description, grade, image_list, introduction, isbn, name, original_price, price, publisher, _id, college } = JSON.parse(options.message)
       for (var i = 0; i < image_list.length; i++) {
         this.data.showList.push({
           url: image_list[i],
           isImage: true,
         })
       }
+      var showListToo = this.data.showList.map(i => (i))
       this.setData({
         userOpenid: __user.getUserOpenid(),
         showState: false,
@@ -116,6 +118,7 @@ Page({
         description: description,
         grade: grade,
         showList: this.data.showList,
+        showListToo: showListToo,
         introduction: introduction,
         isbn: isbn,
         originalPrice: original_price,
@@ -251,29 +254,37 @@ Page({
 
   // 删除展示列表里的某一张图片
   deleteImage(event) {
-    // 如果该图片不存在，则直接删除
-    if (this.data.showList[event.detail.index].url == undefined) {
+    // 如果是修改书籍页面
+    if (!this.data.showState) {
       this.data.showList.splice(event.detail.index, 1)
       this.setData({
         showList: this.data.showList
       })
-      return
-    }
-    if (this.data.showList[event.detail.index].url.slice(0, 8) === 'cloud://')
-      wx.cloud.deleteFile({
-        fileList: [this.data.showList[event.detail.index].url]
-      })
-        .then(res => {
-          this.data.showList.splice(event.detail.index, 1)
-          this.setData({
-            showList: this.data.showList
-          })
+    } else {    // 不是修改书籍页面
+      // 如果该图片不存在，则直接删除
+      if (this.data.showList[event.detail.index].url == undefined) {
+        this.data.showList.splice(event.detail.index, 1)
+        this.setData({
+          showList: this.data.showList
         })
-    else {
-      this.data.showList.splice(event.detail.index, 1)
-      this.setData({
-        showList: this.data.showList
-      })
+        return
+      }
+      if (this.data.showList[event.detail.index].url.slice(0, 8) === 'cloud://')
+        wx.cloud.deleteFile({
+          fileList: [this.data.showList[event.detail.index].url]
+        })
+          .then(res => {
+            this.data.showList.splice(event.detail.index, 1)
+            this.setData({
+              showList: this.data.showList
+            })
+          })
+      else {
+        this.data.showList.splice(event.detail.index, 1)
+        this.setData({
+          showList: this.data.showList
+        })
+      }
     }
   },
 
@@ -459,11 +470,26 @@ Page({
 
   //更新上传实时的我的卖书信息，通过选择判断决定是否进行上传更新我的卖书信息
   upDateMySellBooksMessages() {
-    this.fromObjectToChar()
-    // 将临时图片路径转换为云存储文件ID
     wx.showLoading({
       title: '修改中',
     })
+    // 将已删除的数据清除
+    this.data.showListToo.forEach(i => {
+      var isDelete = true
+      this.data.showList.forEach(item => {
+        if (i.url === item.url)
+          isDelete = false
+      })
+      if (isDelete && i.url.slice(0, 8) === 'cloud://')    // 如果需要删除
+      {
+        wx.cloud.deleteFile({
+          fileList: [i.url]
+        })
+      }
+    })
+
+    this.fromObjectToChar()
+    // 将临时图片路径转换为云存储文件ID
     const uploadTasks = this.data.imageList.map((i, idx) => {
       if (i.slice(0, 11) === 'http://tmp/' || i.slice(0, 12) === 'wxfile://tmp') {
         return this.uploadFilePromise(new Date().getTime(), idx, __user.getUserOpenid(), i.slice(-4), i)
