28d07cf2fb68181cb3feb184b994c8c2cce98cdc
葛震鹏 <11231780+gezhenpeng@user.noreply.gitee.com>
2022-08-28 15:45:15 +0800

    refactor: encapsulate a function

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -15,22 +15,30 @@ App({
     this.globalData.userInfo = wx.getStorageSync('user').userInfo
     this.globalData.userOpenid = wx.getStorageSync('user').userOpenid
 
-    // 检测更新
+    this.update()
+  },
+
+  // 更新为最新版本
+  update() {
     const updateManager = wx.getUpdateManager()
+
     updateManager.onCheckForUpdate(function (res) {
-      console.log(res.hasUpdate)
+      // 请求完新版本信息的回调
+      console.log(res)
     })
+
     updateManager.onUpdateReady(function () {
       wx.showModal({
         title: '更新提示',
-        content: '新的版本下载已经完成，是否立即更新至最新版本？',
+        content: '新版本已经准备好，是否重启应用？',
         success: function (res) {
           if (res.confirm) {
             updateManager.applyUpdate()
           } else {
             wx.showModal({
               title: '取消提醒',
-              content: '我们建议您进行更新，以便获得更好的体验哦~'
+              content: '我们建议您进行更新，以便获得更好的体验哦~',
+              confirmText: '更新'
             }).then(resner => {
               if (resner.confirm) {
                 updateManager.applyUpdate()
@@ -38,6 +46,14 @@ App({
             })
           }
         }
+      })
+    })
+
+    // 新版本下载失败
+    updateManager.onUpdateFailed(function () {
+      wx.showToast({
+        title: '新版本下载失败，建议检查网络等设置',
+        icon: 'none',
       })
     })
   },
