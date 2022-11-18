5df2ddde5d04e2ca7c772afc56a632994de1cbdc
葛震鹏 <11231780+gezhenpeng@user.noreply.gitee.com>
2022-11-18 22:31:45 +0800

    fix: solve the problem that the avatar cannot be saved

--- miniprogram/pages/personalInformation/personalInformation.js
+++ miniprogram/pages/personalInformation/personalInformation.js
@@ -3,15 +3,18 @@ const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia0
 
 Page({
   data: {
-    avatarUrl: defaultAvatarUrl,
+    avatarUrl: [defaultAvatarUrl],    // 保存为仅含一个元素的数组方便之后换取永久路径
     nickname: '',
+    userOpenid: '',
   },
 
   onLoad: function (option) {
     const eventChannel = this.getOpenerEventChannel()
-    // 监听 getOpenid 事件，获取上一页面通过 eventChannel 传送到当前页面的数据
-    eventChannel.on('getOpenid', function (userOpenid) {    // 获取“我的”页面传递而来的userOpenid信息
-      console.log(userOpenid)
+    var that = this                                  // 获取页面实例
+    eventChannel.on('getOpenid', function (res) {    // 获取“我的”页面传递而来的userOpenid信息
+      that.setData({
+        userOpenid: res.userOpenid                   // 保存userOpenid为本地变量
+      })
     })
   },
 
@@ -20,7 +23,15 @@ Page({
     console.log(e)
     const { avatarUrl } = e.detail
     this.setData({
-      avatarUrl,
+      avatarUrl: [avatarUrl]
+    })
+  },
+
+  // 将图片上传至云存储
+  uploadFilePromise(_openid, format, filePath) {
+    return wx.cloud.uploadFile({
+      cloudPath: 'avatar/' + _openid + format,
+      filePath: filePath
     })
   },
 
@@ -30,19 +41,30 @@ Page({
     this.setData({
       nickname,
     })
-    if (!this.data.nickname) wx.showToast({
+    if (!this.data.nickname) wx.showToast({                       // 昵称为空
       title: '昵称不能为空',
       icon: 'none'
     })
-    else {
-      const eventChannel = this.getOpenerEventChannel()   // 获取页面间事件通信通道
-      eventChannel.emit('getAvatarAndNickname', {   // 向“我的”页面返回头像与昵称信息
-        userInfo: {
-          avatarUrl: this.data.avatarUrl,
-          nickName: this.data.nickname
+    else {                                                        // 昵称不空
+      const avatarTasks = this.data.avatarUrl.map(i => {          // 将临时路径换为永久路径
+        if (i.slice(0, 11) === 'http://tmp/' || i.slice(0, 12) === 'wxfile://tmp') {
+          return this.uploadFilePromise(this.data.userOpenid, i.slice(i.lastIndexOf(".") - i.length), i)            // 返回云存储的路径
+        } else {
+          return i        // 返回原本路径
         }
-      })
-      wx.navigateBack() // 返回上一页面
+      });
+      Promise.all(avatarTasks)
+        .then(res => {
+          const eventChannel = this.getOpenerEventChannel()   // 获取页面间事件通信通道
+          eventChannel.emit('getAvatarAndNickname', {         // 向“我的”页面返回头像与昵称信息
+            userInfo: {
+              avatarUrl: res[0].fileID,
+              nickName: this.data.nickname
+            }
+          })
+          wx.navigateBack()     // 返回上一页面
+        })
+        .catch(err => console.error(err))
     }
   },
 })
